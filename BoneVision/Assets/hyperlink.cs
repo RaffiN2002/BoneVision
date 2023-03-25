ecafd8997b9d1634344be291e55b74d43c3b56fb
RaffiN2002 <122261094+RaffiN2002@users.noreply.github.com>
2023-03-25 19:24:55 +0700

    Build settings change

---
+++ BoneVision/Assets/hyperlink.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hyperlink : MonoBehaviour
+{
+    public void toAR(){
+        SceneManager.LoadScene("AR");
+    }
+
+    public void exitButton(){
+        Application.Quit();
+        Debug.Log("Game Close");
+    }
+
+    public void aboutButton(){
+        SceneManager.LoadScene("About");
+    }
+}
